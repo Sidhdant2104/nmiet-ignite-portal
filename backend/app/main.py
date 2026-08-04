31e71483d48c0d7d282eebc3016ecd28b843a7dc
Sid <71384281+Sidhdant2104@users.noreply.github.com>
2026-08-04 18:16:04 +0530

    cors solved

--- backend/app/main.py
+++ backend/app/main.py
@@ -44,6 +44,7 @@ app.add_middleware(
         "http://127.0.0.1:8080",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://nmietsihportal.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
